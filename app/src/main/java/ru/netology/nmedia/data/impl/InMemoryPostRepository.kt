e96323b0090b487beec62325e337b122e2187113
stor5d <stor5d@mail.ru>
2022-05-10 23:12:37 +0300

    Add CRUD

--- app/src/main/java/ru/netology/nmedia/data/impl/InMemoryPostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/data/impl/InMemoryPostRepository.kt
@@ -6,20 +6,22 @@ import ru.netology.nmedia.data.PostRepository
 
 class InMemoryPostRepository : PostRepository {
 
+    private var nextId = GENERETED_POST_AMOUNT.toLong()
+
     private val posts
         get() = checkNotNull(data.value) {
             "Не нуль"
         }
 
     override val data = MutableLiveData(
-        List(10) { index ->
+        List(GENERETED_POST_AMOUNT) { index ->
             Post(
                 id = index + 1L,
                 author = "Netology",
                 content = "Текст поста $index",
                 published = "07.05.2022",
                 viewCount = 99,
-                likes = 99,
+                likes = 999,
                 shareCount = 99
             )
         }
@@ -44,5 +46,27 @@ class InMemoryPostRepository : PostRepository {
                 post.copy(shareCount = post.shareCount + 1)
             }
         }
+    }
+
+    override fun delete(postId: Long) {
+        data.value = posts.filterNot { post -> post.id == postId }
+    }
+
+    override fun save(post: Post) {
+        if (post.id == PostRepository.NEW_POST_ID) insert(post) else update(post)
+    }
+
+    private fun insert(post: Post) {
+        data.value = listOf(post.copy(id = ++nextId)) + posts
+    }
+
+    private fun update(post: Post) {
+        data.value = posts.map {
+            if (post.id == it.id) post else it
+        }
+    }
+
+    companion object {
+        const val GENERETED_POST_AMOUNT = 1000
     }
 }
