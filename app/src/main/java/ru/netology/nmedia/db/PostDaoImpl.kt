d41e80f2c57001c08241883387d9a469bf841d56
stor5d <stor5d@mail.ru>
2022-05-31 10:24:47 +0300

    SQL prev commit

--- app/src/main/java/ru/netology/nmedia/db/PostDaoImpl.kt
+++ app/src/main/java/ru/netology/nmedia/db/PostDaoImpl.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.db
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import ru.netology.nmedia.post.Post
 
@@ -14,18 +15,60 @@ class PostDaoImpl(
         "${PostTable.Column.ID.columnName} DESC"
     ).use { cursor ->
         List(cursor.count) {
-            TODO("Not yet implemented")
+            cursor.moveToNext()
             cursor.toPost()
         }
     }
 
 
     override fun save(post: Post): Post {
-        TODO("Not yet implemented")
+        val values = ContentValues().apply {
+            put(PostTable.Column.AUTHOR.columnName, post.author)
+            put(PostTable.Column.CONTENT.columnName, post.content)
+            put(PostTable.Column.PUBLISHED.columnName, post.published)
+            put(PostTable.Column.LIKED_BY_ME.columnName, post.likeByMe)
+            put(PostTable.Column.LIKES.columnName, post.likes)
+            put(PostTable.Column.SHARE_COUNT.columnName, post.shareCount)
+            put(PostTable.Column.VIEW_COUNT.columnName, post.viewCount)
+            put(PostTable.Column.URL_VIDEO.columnName, post.urlVideo)
+        }
+        val id = if (post.id != 0L) {
+            db.update(
+                PostTable.NAME,
+                values,
+                "${PostTable.Column.ID.columnName} =?",
+                arrayOf(post.id.toString())
+            )
+            post.id
+        } else {
+            db.insert(
+                PostTable.NAME,
+                null,
+                values
+            )
+        }
+        return db.query(
+            PostTable.NAME,
+            PostTable.ALL_COLUMNS_NAME,
+            "${PostTable.Column.ID.columnName} =?",
+            arrayOf(id.toString()),
+            null, null, null
+        ).use { cursor ->
+            cursor.moveToNext()
+            cursor.toPost()
+        }
     }
 
+
     override fun likeById(postId: Long) {
-        db.execSQL()
+        db.execSQL(
+            """
+           UPDATE ${PostTable.NAME} SET
+              ${PostTable.Column.LIKES.columnName} = ${PostTable.Column.LIKES.columnName} + CASE WHEN ${PostTable.Column.LIKED_BY_ME.columnName} THEN -1 ELSE 1 END,
+               ${PostTable.Column.LIKED_BY_ME.columnName} = CASE WHEN ${PostTable.Column.LIKED_BY_ME.columnName} THEN 0 ELSE 1 END
+           WHERE ${PostTable.Column.ID.columnName} = ?;
+        """.trimIndent(), arrayOf(postId)
+        )
     }
 
     override fun removeById(postId: Long) {
